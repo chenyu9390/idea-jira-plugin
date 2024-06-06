b768ba40d710c8eea7759177d6e10aedac0986b9
albert.chen <albert.chen@quectel.com>
2024-06-06 10:21:23 +0800

    动态下拉选

--- src/main/java/com/ck/window/JiraWindow.java
+++ src/main/java/com/ck/window/JiraWindow.java
@@ -1,6 +1,7 @@
 package com.ck.window;
 
 import cn.hutool.core.util.StrUtil;
+import com.ck.dot.TransitionDto;
 import com.intellij.ide.todo.TodoView;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.DumbService;
@@ -28,6 +29,7 @@ import java.net.URI;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static javax.swing.text.StyleConstants.getComponent;
 
@@ -37,45 +39,11 @@ public class JiraWindow implements ToolWindowFactory {
     private final JiraService jiraService = new JiraService(jiraConfig);
     private static final Logger LOGGER = LoggerFactory.getLogger(JiraWindow.class);
 
-    public void createToolWindowContent1(Project project, ToolWindow toolWindow) {
-        // 创建表格数据
-        Object[][] data = {
-                {"Bug001", "John", 25, "Male"},
-                {"Bug002", "Jane", 30, "Female"},
-                {"Bug003", "Doe", 40, "Male"}
-        };
-
-        // 创建表格列名
-        String[] columns = {"Bug ID", "Name", "Age", "Gender"};
-
-        // 创建默认表格模型
-        DefaultTableModel model = new DefaultTableModel(data, columns);
-
-        // 创建 JTable
-        JTable table = new JTable(model);
-
-        // 为 "Gender" 列创建一个下拉选择框
-        JComboBox<String> comboBox = new JComboBox<>(new String[]{"Male", "Female", "Other"});
-        TableColumn genderColumn = table.getColumnModel().getColumn(3); // Gender 列索引
-        genderColumn.setCellEditor(new DefaultCellEditor(comboBox));
-
-        // 创建 JScrollPane 来容纳 JTable，使得可以滚动显示
-        JScrollPane scrollPane = new JScrollPane(table);
-
-        // 创建内容面板并添加 JScrollPane
-        JPanel panel = new JPanel(new BorderLayout());
-        panel.add(scrollPane, BorderLayout.CENTER);
-
-        // 创建内容并将其添加到工具窗口
-        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
-        Content content = contentFactory.createContent(panel, "", false);
-        toolWindow.getContentManager().addContent(content);
-    }
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
         // 创建表格列名
-        String[] columns = {"BugId", "Summer","解决方案","修复版本","影响范围","提交"};
+        String[] columns = {"BugId", "Summer","Transaction","解决方案","修复版本","影响范围","解决"};
         List<JiraVo> issueList = jiraService.getIssueList();
-        Object[][] data = new Object[issueList.size()][6];
+        Object[][] data = new Object[issueList.size()][7];
         if (!issueList.isEmpty()) {
             for (int i = 0; i < issueList.size(); i++) {
                 data[i][0] = issueList.get(i).getBugId();
@@ -86,11 +54,8 @@ public class JiraWindow implements ToolWindowFactory {
         DefaultTableModel model = new DefaultTableModel(data,columns);
         // 创建 JTable
         JTable table = new JTable(model);
-        // 为 "Gender" 列创建一个下拉选择框
-        JComboBox<String> comboBox = new JComboBox<>(new String[]{"无", "已解决", "不修复", "延迟修复", "重复的BUG", "无效的BUG"});
-        table.getColumnModel().getColumn(2).setCellEditor(new DefaultCellEditor(comboBox));
-        table.getColumnModel().getColumn(5).setCellEditor(new CustomCellEditor());
-        table.getColumnModel().getColumn(5).setCellRenderer(new CustomCellRenderer());
+        repairPlan(table);
+        dynamicTransitions(issueList,table);
         // 创建 JScrollPane 来容纳 JTable，使得可以滚动显示
         JScrollPane scrollPane = new JScrollPane(table);
 
@@ -113,6 +78,12 @@ public class JiraWindow implements ToolWindowFactory {
                 // 获取点击的行和列索引
                 int row = table.rowAtPoint(e.getPoint());
                 int col = table.columnAtPoint(e.getPoint());
+                CustomTableModel model = (CustomTableModel) table.getModel();
+                StringBuilder rowData = new StringBuilder();
+                for (int cl = 0; cl < model.getColumnCount(); cl++) {
+                    rowData.append(model.getValueAt(row, cl)).append(" ");
+                }
+                System.out.println(rowData.toString());
                 if (col == 0) {
                     // 获取点击的单元格数据
                     String bugId = (String) table.getValueAt(row, col);
@@ -135,21 +106,18 @@ public class JiraWindow implements ToolWindowFactory {
         toolWindow.getContentManager().addContent(content);
     }
 
-    public void createToolWindowContent2(@NotNull Project project, @NotNull ToolWindow toolWindow){
-        JFrame frame = new JFrame("Custom Table Example");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    /**
+     * 修复方案
+     */
+    private void repairPlan(JTable table){
+        // 为 "Gender" 列创建一个下拉选择框
+        JComboBox<String> comboBox = new JComboBox<>(new String[]{"无", "已解决", "不修复", "延迟修复", "重复的BUG", "无效的BUG"});
+        table.getColumnModel().getColumn(2).setCellEditor(new DefaultCellEditor(comboBox));
+    }
 
-        JTable table = new JTable(new CustomTableModel());
-        table.setRowHeight(30);
-
-        TableColumnModel columnModel = table.getColumnModel();
-        columnModel.getColumn(1).setCellEditor(new CustomCellEditor());
-        columnModel.getColumn(1).setCellRenderer(new CustomCellRenderer());
-
-        frame.add(new JScrollPane(table));
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+    private void dynamicTransitions(List<JiraVo> issueList,JTable table) {
+        table.getColumnModel().getColumn(3).setCellEditor(new CustomCellEditor(issueList));
+        table.getColumnModel().getColumn(3).setCellRenderer(new CustomCellRenderer(issueList));
     }
 }
 
@@ -196,19 +164,11 @@ class CustomTableModel extends AbstractTableModel {
 
 class CustomCellEditor extends AbstractCellEditor implements TableCellEditor {
     private final Map<Integer, JComboBox<String>> comboBoxes = new HashMap<>();
-
     private JComboBox<String> currentComboBox;
-
-    public CustomCellEditor() {
-        String[] options1 = {"Option 1A", "Option 1B", "Option 1C"};
-        String[] options2 = {"Option 2A", "Option 2B", "Option 2C"};
-        String[] options3 = {"Option 3A", "Option 3B", "Option 3C"};
-        String[] options4 = {"Option 4A", "Option 4B", "Option 4C"};
-
-        comboBoxes.put(0, new JComboBox<>(options1));
-        comboBoxes.put(1, new JComboBox<>(options2));
-        comboBoxes.put(2, new JComboBox<>(options3));
-        comboBoxes.put(3, new JComboBox<>(options4));
+    public CustomCellEditor(List<JiraVo> issueList) {
+        for (int i = 0; i < issueList.size(); i++) {
+            comboBoxes.put(i, new JComboBox<>(issueList.get(i).getDtos().stream().map(TransitionDto::getName).toArray(String[]::new)));
+        }
     }
 
     @Override
@@ -224,29 +184,16 @@ class CustomCellEditor extends AbstractCellEditor implements TableCellEditor {
     }
 }
 class CustomCellRenderer extends JComboBox<String> implements TableCellRenderer {
-    public CustomCellRenderer() {
-        super();
+    private List<JiraVo> issueList;
+    public CustomCellRenderer(List<JiraVo> issueList) {
+        this.issueList = issueList;
     }
 
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         removeAllItems();
-        if (row == 0) {
-            addItem("Option 1A");
-            addItem("Option 1B");
-            addItem("Option 1C");
-        } else if (row == 1) {
-            addItem("Option 2A");
-            addItem("Option 2B");
-            addItem("Option 2C");
-        } else if (row == 2) {
-            addItem("Option 3A");
-            addItem("Option 3B");
-            addItem("Option 3C");
-        } else if (row == 3) {
-            addItem("Option 4A");
-            addItem("Option 4B");
-            addItem("Option 4C");
+        for (int i = 0; i < issueList.size(); i++) {
+            issueList.get(i).getDtos().stream().map(TransitionDto::getId).forEach(this::addItem);
         }
         setSelectedItem(value);
         return this;
